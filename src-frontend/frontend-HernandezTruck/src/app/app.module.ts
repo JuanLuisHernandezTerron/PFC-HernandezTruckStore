e607cef78621b298aebf80fbf5b515149103819a
unknown <you@example.com>
2023-03-28 19:16:07 +0200

    front componentes

--- src-frontend/frontend-HernandezTruck/src/app/app.module.ts
+++ src-frontend/frontend-HernandezTruck/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MaterialModule } from './material/material.module';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    SkeletonComponent
   ],
   imports: [
     BrowserModule,
