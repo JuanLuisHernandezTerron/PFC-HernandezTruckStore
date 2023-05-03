d8627556cb435e3172a540e753d7de88cf7dc8f0
unknown <you@example.com>
2023-05-03 17:23:16 +0200

    cambios

--- src-frontend/frontend-HernandezTruck/src/app/components/mainInformacion/Tractora/tractora-informacion/tractora-informacion.component.ts
+++ src-frontend/frontend-HernandezTruck/src/app/components/mainInformacion/Tractora/tractora-informacion/tractora-informacion.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PostService } from 'src/app/services/Post/post.service';
 import { PostVehicle } from 'src/app/models/PostVehiculo';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -11,46 +11,46 @@ import {Observable} from 'rxjs';
 })
 export class TractoraInformacionComponent {
   constructor(private servicePost: PostService,
-              ){ }
+  ) { }
   post: PostVehicle;
+  arraydatos: Array<PostVehicle> = [];
 
-  ngOnInit(){
-    this.servicePost.getPostsVehicle().subscribe((data)=>{
+  ngOnInit() {
+    this.servicePost.getPostsVehicle().subscribe((data) => {
       this.post = data;
-      console.log(this.post)
-    })
-  }
-
-  getPostAlquilerTractora(){
-    this.servicePost.getPostAlquiler().subscribe((data)=>{
-      let contador = 0;
       data.forEach(v => {
-        if (v.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora"){
-          contador ++;
-          this.post = data
-        }else{
-          data.splice(contador,contador+1)
-        };
+        if (v.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora") {
+          this.arraydatos.push(v)
+        }
       });
     })
   }
 
-  getPostCompraTractora(){
-    this.servicePost.getPostVenta().subscribe((data)=>{
-      let contador = 0;
+  getPostAlquilerTractora() {
+    this.arraydatos = [];
+    this.servicePost.getPostAlquiler().subscribe((data) => {
+      console.log(data)
       data.forEach(v => {
-        if (v.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora"){
-          contador ++;
-          this.post = data
-        }else{
-          data.splice(contador,contador+1)
-        };
+        if (v.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora" && v.tipo_publicacion === "Alquilar") {
+          this.arraydatos.push(v)
+        }
       });
     })
   }
 
-  idBoton(e){
+  getPostCompraTractora() {
+    this.arraydatos = [];
+    this.servicePost.getPostVenta().subscribe((data) => {
+      data.forEach(v => {
+        if (v.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora" && v.tipo_publicacion === "Vender") {
+          this.arraydatos.push(v)
+        }
+      });
+    })
+  }
+
+  idBoton(e) {
     console.log(e)
-    return e; 
+    return e;
   }
 }
