0806cbb73141caefeb8b438487423a0be7343619
unknown <you@example.com>
2023-05-07 17:26:50 +0200

    mejorasFront

--- src-frontend/frontend-HernandezTruck/src/app/components/informacionDetalladaPost/registrado/register-detallado/register-detallado.component.ts
+++ src-frontend/frontend-HernandezTruck/src/app/components/informacionDetalladaPost/registrado/register-detallado/register-detallado.component.ts
@@ -2,40 +2,67 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '././../../../../services/auth.service';
 import { PostService } from 'src/app/services/Post/post.service';
 import { TractoraService } from 'src/app/services/Vehiculos/Tractora/tractora.service';
-
+import { PostVehicle } from 'src/app/models/PostVehiculo';
+import { tractora } from 'src/app/models/tractora';
 @Component({
   selector: 'app-register-detallado',
   templateUrl: './register-detallado.component.html',
   styleUrls: ['./register-detallado.component.scss']
 })
 export class RegisterDetalladoComponent implements OnInit {
-  arraydatos !: any;
+  informacionPost !: PostVehicle;
   es_tractora = false;
-  arrayDatoTractora !: any; 
-
+  DatoTractora !: tractora; 
   constructor (public authservice: AuthService,
               private postServie : PostService,
               private tractoraService: TractoraService) { }
   contador = 0;
+
   ngOnInit(): void {
+    this.informacionPost = {
+      _id:'',
+      titulo:'',
+      fecha_post: new Date(),
+      tipo_publicacion:'',
+      Reports: [],
+      likes:[],
+      media:'',
+      informacionUser:[]
+    }
+
+    this.DatoTractora={
+      _id:'',
+      cv:0,
+      adblue: false,
+      numeroDepositos: 0,
+      kms:0,
+      combustible:'',
+      retarder:false,
+      vehiculo:[]
+    }
+
+
     let slug = window.location.pathname.split("/")
     this.postServie.getPost(slug[2]).subscribe((data) => {
-      this.arraydatos = data;
+      this.informacionPost = data;
     })
 
-    // this.tractoraService.getInfoVehiculo(this.arraydatos.informacionUser[0].idVehiculo._id).subscribe((data)=>{
-    //   return data;
-    //   })
   }
 
   tipoPost(){
    let esCabeza = false;
-  if (this.arraydatos.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora") {
-       esCabeza =  true;
-       console.log(this.arraydatos)
-  }else{
 
-  }
+    if (this.informacionPost.informacionUser[0].idVehiculo.tipoVehiculo === "cabezatractora") {
+      esCabeza =  true;
+      if (this.contador <= 0) {
+         this.contador ++;
+         this.tractoraService.getInfoVehiculo(this.informacionPost.informacionUser[0].idVehiculo._id).subscribe((data)=>{
+         this.DatoTractora = data
+      })
+      }
+      console.log(this.informacionPost)
+      console.log(this.DatoTractora)
+   }
     return esCabeza;
   }
 }
